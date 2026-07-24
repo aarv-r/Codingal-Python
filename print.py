ee19d2549be1e3073e81e3986b8fb61c88c1ab74
aarv-r <47905296+aarv-r@users.noreply.github.com>
2026-07-24 19:06:52 +1000

    Add files via upload

---
+++ print.py
@@ -0,0 +1,5 @@
+print ("My name is Aarav")
+ # This print will state how many chocolates I have in my pocket. 
+print ("I have", 12 ,"chocolates in my pocket")
+ # This print will show the result of adding two numbers.
+print ('the result of', 3 ,'and', 5 ,'is', 3 + 5)
