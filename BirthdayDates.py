1ff1038326400186c37a91bef2bfcae71c76bb61
aarv-r <47905296+aarv-r@users.noreply.github.com>
2026-08-05 21:00:13 +1000

    Add files via upload

---
+++ BirthdayDates.py
@@ -0,0 +1,12 @@
+Friend_Birthday1= "11/12/2005"
+Friend_Birthday2= "5/7/2006"
+Friend_Birthday3= "2/9/2005"
+Friend_Birthday4= "3/12/2006"
+Friend_Birthday5= "1/1/2005"
+
+print("Raj's Friend's birthdays are:")
+print("Friend1", Friend_Birthday1)
+print("Friend2", Friend_Birthday2)
+print("Friend3", Friend_Birthday3)
+print("Friend4", Friend_Birthday4)
+print("Friend5", Friend_Birthday5)
