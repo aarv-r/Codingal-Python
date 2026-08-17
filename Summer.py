a085afb9de6bdccd5665e5b0dd6d656cca68eaf4
aarv-r <47905296+aarv-r@users.noreply.github.com>
2026-08-17 20:51:41 +1000

    Add files via upload

---
+++ Summer.py
@@ -0,0 +1,7 @@
+choice=input("Enter weather temperature here: ")
+if choice <"23":
+    print("You should wear a jacket")
+elif choice=="23":
+    print("You can wear either a jacket or a t-shirt")
+else:
+    print("You can wear a t-shirt")
